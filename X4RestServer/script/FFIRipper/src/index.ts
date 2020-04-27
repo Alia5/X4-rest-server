34a7966646cd194f13685867952f4bfb8ef07ee1
Peter Repukat <orly.alia5@gmail.com>
2020-04-27 18:11:38 +0200

    FFIRipper sort structs by usage + OSX_Workaround
    wtf

--- X4RestServer/script/FFIRipper/src/index.ts
+++ X4RestServer/script/FFIRipper/src/index.ts
@@ -8,26 +8,33 @@ import { promisify } from 'util';
 const readFile = promisify(readFileCallbacks);
 
 
-const ffi_typedef_h =
+const FFI_TYPEDEF_H =
 `#pragma once
 #include <cstdint>
 
 namespace X4FFI
-`;
+` as const;
 
-const ffi_typedef_struct_h =
+const FFI_TYPEDEF_STRUCT_H =
 `#pragma once
 #include "ffi_typedef.h"
 
 namespace X4FFI
-`;
+` as const;
 
-const ffi_funcs_h =
+const FFI_FUNCS_H =
 `#pragma once
 #include "fii_typedef_struct.h"
 
 namespace X4FFI
-`;
+` as const;
+
+const OSX_WORKAROUND = [
+    'typedef struct {\n    int unknown;\n} mach_header;',
+    'typedef struct {\n    int unknown;\n} mach_header_64;',
+    'typedef struct {\n    int unknown;\n} mach_fat_header;',
+    'typedef struct {\n    int unknown;\n} mach_fat_arch;'
+];
 
 
 namespace FFIRipper {
@@ -72,9 +79,12 @@ namespace FFIRipper {
         // remove duplicates
         .filter((v,i,arr) => arr.indexOf(v) === i);
 
-    const filterStructs = (ffiFiles: string[]): string[] => ffiFiles.flatMap((ffiStrings) => ffiStrings.split(/(?<=\}(.*?);)/g))
+    const filterStructs = (ffiFiles: string[]): string[] => ffiFiles.join('\n')
+        // as ecmascript doesn't have recursive regexes, here a massive monster for parsing structs
+        .match(/typedef struct(\s|\n)(\{(?:[^}{]*((\{(?:[^}{]*((\{(?:[^}{]*((\{(?:[^}{]*()?)*\}))?)*\}))?)*\}))?)*\}).*?;/gm)
         // remove blank lines
-        .map((ffiString) => ffiString.replace(/^\s*/g, ''))
+        .map((ffiString) => ffiString.replace(/^(\s|\n)*/g, ''))
+        .map((ffiString) => ffiString.replace(/^typedef struct\s*?\n/g, 'typedef struct '))
         .filter((ffiString) => ffiString.startsWith('typedef struct'))
         // remove const variants
         .filter((v, i, arr) =>
@@ -97,26 +107,52 @@ namespace FFIRipper {
         // remove dupes with different parameter names (and potential overloads, but meh);
         .filter((v, i, arr) => arr[i+1]?.split('(')?.[0] !== v?.split('(')?.[0]);
 
+    const sortStructArray = (structs: string[]): string[] => {
+        // This bloody hell of a mess needs to be fixed.
+        let requiredTypes: string[] = [];
+        structs.forEach((struct) => {
+            const reqTypes = struct.split(/(?<=;)/g)
+                .filter((line) => !line.includes('{') && !line.includes('}') && !line.includes('const'))
+                .flatMap((line) => line.match(/\S+?(?=\s\S)/gm))
+                .map((type) => type.replace(/\*/g, ''))
+                .sort()
+                .filter((v,i,arr) => arr.indexOf(v) === i) // remove dupes
+                .filter((type) => !type.match(/(int(32|64)|float|bool|char|double|long)/g))
+                // just hardcode normal typedefs. i doubt they need to get updated all the time...
+                .filter((type) => !type.match(/(BlacklistID|BuildTaskID|MissionID|NPCSeed|TradeID|MessageID|UniverseID)/g));
+            if (reqTypes.length > 0) {
+                requiredTypes.splice(Number.MAX_VALUE, 0, ...reqTypes);
+            }
+        });
+        requiredTypes = requiredTypes.sort().filter((v,i,arr) => arr.indexOf(v) === i);
+        console.log(requiredTypes);
+        const res = structs.sort((structA, structB) =>
+            requiredTypes.includes(structA.match(/\S+?(?=;(?!\s))/g)?.[0])
+                ? -1 : requiredTypes.includes(structB.match(/\S+?(?=;(?!\s))/g)?.[0])
+                    ? 1 : structA > structB ? -1 : 1);
+        res.unshift(...OSX_WORKAROUND);
+        return res;
+    };
+
+
     export const main = async (): Promise<void> => {
 
         const luaFiles = await getFfiLuaFiles();
         const ffiStrings = await readFFiStrings(luaFiles);
 
         const typedefs = filterTypedefs(ffiStrings);
-        const structs = filterStructs(ffiStrings);
+        const structs = sortStructArray(filterStructs(ffiStrings));
         const funcs = filterFuns(ffiStrings);
 
-        // TODO: sort structs by usage;
         // TODO: transform function decls to types
-
 
         writeFileSync(
             resolve(join(__dirname, '../../../X4RestServer/src/__generated__/ffi_typedef.h')),
-            `${ffi_typedef_h}{\n    ${typedefs.join('\n    ')}\n}`
+            `${FFI_TYPEDEF_H}{\n    ${typedefs.join('\n    ')}\n}`
         );
         writeFileSync(
             resolve(join(__dirname, '../../../X4RestServer/src/__generated__/ffi_typedef_struct.h')),
-            `${ffi_typedef_struct_h}{\n    ${structs.join('\n').replace(/\n/g, '\n    ')}\n}`
+            `${FFI_TYPEDEF_STRUCT_H}{\n    ${structs.join('\n').replace(/\n/g, '\n    ')}\n}`
         );
         // writeFileSync('../../../X4RestServer/src/__generated__/ffi_typedef.h', `${ffi_typedef_h}{    ${typedefs.join('\n    ')}}`);
 
@@ -129,4 +165,3 @@ namespace FFIRipper {
 }
 
 FFIRipper.main();
-
